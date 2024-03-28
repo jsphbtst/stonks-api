0ab34967727a65c1bc4e81e02858e84cbb2394b2
Joseph J. Bautista <joseph.bautista@proton.me>
2024-03-28 22:15:01 +0100

    feat: added snp500 script

--- apps/scripts/src/index.ts
+++ apps/scripts/src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello World')
+async function main() {
+  const file = Bun.file('apps/scripts/src/assets/snp500.csv')
+  const text = await file.text()
+  const textSplit = text.split('\n')
+
+  const regex = /(".*?"|[^,]+)/g
+  const tickers: string[] = []
+  for (let idx = 1; idx < textSplit.length; idx++) {
+    const row = textSplit[idx].match(regex)
+    if (!row) {
+      console.error('Could not split: ', textSplit[idx])
+      continue
+    }
+
+    const symbol = row[0]
+    tickers.push(symbol)
+  }
+
+  const sortedTickers: string[] = tickers.sort()
+  console.log('Total tickers parsed: ', sortedTickers.length)
+  console.log(sortedTickers)
+}
+
+main()
